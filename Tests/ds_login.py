0188a3eb37362004870a382e4434e7d91d000bb0
NHN-Commerce-QA <seunghoon.yoo@nhnservice.com>
2022-12-28 17:41:45 -0800

    ds-login code refactoring #FG-Test-automation/4

--- Tests/ds_login.py
+++ Tests/ds_login.py
@@ -1,7 +1,7 @@
 import sys, os # 폴더 다를 때 사용
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from Libs import webdriver
-from Pages import ws_front_login
+from Libs import ds_front_login
+
 ## TC_4
 ds_front_login.ds_login()
