a61aaaecc92039d9a416a24b453fc26f8afc973b
NHN-Commerce-QA <seunghoon.yoo@nhnservice.com>
2022-12-28 17:39:37 -0800

    chat code refactoring #FG-Test-automation/6

--- Tests/5_ws_chat.py
+++ Tests/5_ws_chat.py
@@ -1,8 +1,7 @@
 import sys, os # 폴더 다를 때 사용
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from Libs import webdriver
 from Pages import ws_front_chat
 
-## TC_4
+## TC_5
 ws_front_chat.ws_chat()
