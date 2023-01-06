eb125d102f2defada51d55fdeab5a86766210c64
NHN-Commerce-QA <seunghoon.yoo@nhnservice.com>
2023-01-05 20:42:00 -0800

    msgbox module change : #FG-Test-automation/3

--- Libs/ws_front_login.py
+++ Libs/ws_front_login.py
@@ -1,5 +1,5 @@
 import time
-import pyautogui
+from tkinter import messagebox as msgbox
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager 
 from selenium.webdriver.common.keys import Keys
@@ -22,7 +22,7 @@ def ws_login():
         cookiebtn.click()
         time.sleep(1)
     except:
-        pyautogui.alert("Error : Cookie accept all btn is not found")
+        msgbox.showerror("Error", "쿠키 버튼을 찾을 수 없습니다.")
         print("Error : Cookie accept all btn is not found")
         driver.quit()
 
@@ -32,7 +32,7 @@ def ws_login():
         loginbtn.click()
         time.sleep(1)
     except:
-        pyautogui.alert("Error : login btn is not found")
+        msgbox.showerror("Error", "로그인 버튼을 찾을 수 없습니다.")
         print("Error : login btn is not found")
         driver.quit()
 
@@ -46,7 +46,7 @@ def ws_login():
         driver.find_element(By.NAME, "password").send_keys(PW + Keys.TAB)
         time.sleep(0.2)
     except:
-        pyautogui.alert("Error : ID or PW is not found")
+        msgbox.showerror("Error", "ID나 PW를 찾을 수 없습니다.")
         print("Error : ID or PW is not found")
         driver.quit()
 
@@ -56,7 +56,7 @@ def ws_login():
         signin_btn.click()
         driver.implicitly_wait(15)
     except:
-        pyautogui.alert("Error : Sign in btn is not found")
+        msgbox.showerror("Error", "Sign in 버튼을 찾을 수 없습니다.")
         print("Error : Sign in btn is not found")
 
     # 로그인 성공
